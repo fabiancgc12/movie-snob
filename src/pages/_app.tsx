adfd1334e02eef479a44fbca98f3039e604036dc
fabian graterol <fabiancgraterol@gmail.com>
2023-01-27 13:43:20 -0400

    changing favicon and refactoring Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>Popcorn Search</title>
+          <meta name="description" content="Search for your favorite movies and tv shows" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+)
 }
