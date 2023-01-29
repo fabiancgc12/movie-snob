00b52feaa78c09fc42cda96e74087d38bb4f1429
fabian graterol <fabiancgraterol@gmail.com>
2023-01-29 16:25:58 -0400

    creating MainShow.tsx component

---
+++ src/components/MainShow/MainShow.tsx
@@ -0,0 +1,25 @@
+import styles from "@/styles/Home.module.css";
+import Image, {StaticImageData} from "next/image";
+
+type props = {
+    data:{
+        image:string | StaticImageData,
+        title:string,
+        description:string
+    }
+}
+
+export function MainShow({data}:props) {
+    return <div className={styles.mainShow}>
+        <div className={styles.info}>
+            <h2>{data.title}</h2>
+            <p>
+                <small>{data.description}</small>
+            </p>
+            <div className={styles.actions}>
+                <small role="button" className={"contrast"}>More Info</small>
+            </div>
+        </div>
+        <Image src={data.image} alt={"main show poster"}/>
+    </div>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import { MainShow } from "@/components/MainShow/MainShow";
+import { Inter } from '@next/font/google'
+import mainShowImg from "../../public/main_show.jpg"
 
+const inter = Inter({ subsets: ['latin'] })
+
+const show = {
+    image:mainShowImg,
+    title:"Dr strange in the multiverse of madness",
+    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A t e Lorem ipsum dolor sit amet, consectetur" +
+     "adipisicing elit. A accusamus adipisci ducimus facere illo optio praesentium quae qu" +
+    "rerum? Aliquam commodi deserunt eaque hic iusto, maiores pariatur saepe? Consequatur, nesciunt!"
+}
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <main>
+        <MainShow data={show}/>
+    </main>
   )
 }
