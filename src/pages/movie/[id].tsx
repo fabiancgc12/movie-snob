24adba37452a292ff55d057124257610b61566a4
fabian graterol <fabiancgraterol@gmail.com>
2023-02-10 00:29:05 -0400

    fixing styles of movie banner

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -6,7 +6,7 @@ import {BookmarkButton} from "@/components/common/ActionButton/chechMarkButton";
 import {ShareButton} from "@/components/common/ActionButton/ShareButton";
 import {getMovie} from "@/services/movies/getMovie";
 import {MovieInterface} from "@/utils/models/Movies/Movie.interface";
-import {CreditsInterface, CrewEntity} from "@/utils/models/Movies/Credits.interface";
+import {CreditsInterface} from "@/utils/models/Movies/Credits.interface";
 import {generateImageUrl} from "@/utils/functions/generateImageUrl";
 import {calculateRunTime} from "@/utils/functions/calculateRunTime";
 import {CrewMember} from "@/components/credits/CrewMember";
@@ -31,43 +31,45 @@ export default function Movie({movie,credits,trailer,images}:props){
     const director = credits.crew?.find(c => c.job.toLowerCase() == "director")
     return (
         <main>
-            <header className={styles.header}>
-                <div className={styles.poster}>
+            <section>
+                <div className={styles.header}>
+                    <div className={styles.poster}>
+                        <div>
+                            <Image
+                                src={posterPath}
+                                alt={`${movie.title} poster`}
+                                priority
+                                fill
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.banner}>
+                        <Image src={backgroundPath} alt={`${movie.title} backdrop`} className={styles.backdrop} fill/>
+                    </div>
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={styles.extraInfo}>
+                        <div className={styles.genres}>
+                            {movie.genres?.slice(0,3).map(g => <small className={"badge"} key={`genre-${g.id}`}>{g.name}</small>)}
+                        </div>
+                        <div>{calculateRunTime(movie.runtime)}</div>
+                    </div>
+                    <h1 className={styles.title}>{movie.title} <small>({movie.release_date.slice(0,4)})</small></h1>
+                    <div className={styles.overview}>
+                        <h2>Overview</h2>
+                        <p>{movie.overview}</p>
+                    </div>
+                    <div className={`${styles.extraInfo}`}>
+                        <Average value={movie.vote_average}/>
+                        <BookmarkButton/>
+                        <LikeButton/>
+                        <ShareButton/>
+                    </div>
                     <div>
-                        <Image
-                            src={posterPath}
-                            alt={`${movie.title} poster`}
-                            priority
-                            fill
-                        />
+                        {director && <CrewMember people={director} type={"crew"}/>}
                     </div>
                 </div>
-                <div className={styles.banner}>
-                    <Image src={backgroundPath} alt={`${movie.title} backdrop`} className={styles.backdrop} fill/>
-                </div>
-            </header>
-            <div className={styles.wrapper}>
-                <div className={styles.extraInfo}>
-                    <div className={styles.genres}>
-                        {movie.genres?.slice(0,3).map(g => <small className={"badge"} key={`genre-${g.id}`}>{g.name}</small>)}
-                    </div>
-                    <div>{calculateRunTime(movie.runtime)}</div>
-                </div>
-                <h1 className={styles.title}>{movie.title} <small>({movie.release_date.slice(0,4)})</small></h1>
-                <div className={styles.overview}>
-                    <h2>Overview</h2>
-                    <p>{movie.overview}</p>
-                </div>
-                <div className={`${styles.extraInfo}`}>
-                    <Average value={movie.vote_average}/>
-                    <BookmarkButton/>
-                    <LikeButton/>
-                    <ShareButton/>
-                </div>
-                <div>
-                    {director && <CrewMember people={director} type={"crew"}/>}
-                </div>
-            </div>
+            </section>
             <div data-theme="light">
                 <div>
                     <Cast cast={credits.cast}/>
