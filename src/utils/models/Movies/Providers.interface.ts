e840ac84fc4c5b819d0cde411c48250095c259b5
fabian graterol <fabiancgraterol@gmail.com>
2023-02-11 00:07:36 -0400

    adding providers to getMovie.ts

---
+++ src/utils/models/Movies/Providers.interface.ts
@@ -0,0 +1,24 @@
+export interface ProvidersResultInterface {
+    id:      number;
+    results: ProvidersLanguage;
+}
+
+export interface ProvidersLanguage {
+    US: AE;
+}
+
+export interface AE {
+    link:      string;
+    flatrate?: Provider[];
+    buy:       Provider[];
+    rent?:     Provider[];
+    ads?:      Provider[];
+    free?:     Provider[];
+}
+
+export interface Provider {
+    logo_path:        string;
+    provider_id:      number;
+    provider_name:    string;
+    display_priority: number;
+}
