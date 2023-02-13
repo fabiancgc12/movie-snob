57530f222f4e8b071d26295439bcd820765035fc
fabian graterol <fabiancgraterol@gmail.com>
2023-02-12 23:59:57 -0400

    changing overlay of thumbnails

--- src/components/Video/VideoThumbnail.tsx
+++ src/components/Video/VideoThumbnail.tsx
@@ -1,6 +1,6 @@
 import {VideoTrailerInterface} from "@/utils/models/Movies/VideoMedia.interface";
 import { Video } from "./Video";
-import style from "@/components/Video/Video.module.css";
+import styles from "@/components/Video/Video.module.css";
 import Image from "next/image";
 
 type props = {
@@ -12,12 +12,16 @@ export function VideoThumbnail({video}:props){
 
     return (
         <Video video={video}>
-            <div className={style.thumbnail}>
+            <div className={styles.thumbnail}>
                 <Image src={videoThumbnail} alt={`${video.name} thumbnail`} fill/>
-                <div className={style.overlay}>
-                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                <div className={styles.overlay}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
                     </svg>
+                    <span className={styles.title}>
+                        {video.name}
+                    </span>
                 </div>
             </div>
         </Video>
