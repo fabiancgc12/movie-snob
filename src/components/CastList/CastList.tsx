7e148b02c9d4c0d32842616450c6c6a6bf22e1c0
fabian graterol <fabiancgraterol@gmail.com>
2023-02-12 23:31:23 -0400

    changing rendering to a css approach so ssr can shut up about it

--- src/components/CastList/CastList.tsx
+++ src/components/CastList/CastList.tsx
@@ -3,25 +3,18 @@ import styles from "./CastList.module.css";
 import {CrewMemberCard} from "@/components/CrewMember/CrewMemberCard";
 import {Section} from "@/components/common/Section/Section";
 import {Slider} from "@/components/Slider/Slider";
-import useMediaQuery from "@/utils/hooks/useMediaQuery";
 
 type props = {
     cast?:CastEntity[] | null
 }
 export function Cast({cast}:props){
-    const matchBigScreen = useMediaQuery("(min-width: 768px)")
     if (!cast) return null;
-    if (!matchBigScreen)
-        return (
-            <Section title={"Cast"}>
-                <Slider arrowsInContent={true} speed={250}>
-                    {cast.slice(0,12).map(c => <CrewMemberCard key={`cast-${c.id}`} size={"md"} people={c} type={"cast"}/>)}
-                </Slider>
-            </Section>
-        )
     return (
         <Section title={"Cast"}>
-            <div className={styles.cast}>
+            <Slider className={styles.castSm} arrowsInContent={true} speed={250}>
+                {cast.slice(0,12).map(c => <CrewMemberCard key={`cast-${c.id}`} size={"md"} people={c} type={"cast"}/>)}
+            </Slider>
+            <div className={styles.castMd}>
                 {cast.slice(0,12).map(c => <CrewMemberCard key={`cast-${c.id}`} size={"md"} people={c} type={"cast"}/>)}
             </div>
         </Section>
