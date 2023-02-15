c076a11513d47d71e8726c0e96df3ca72ce7def6
fabian graterol <fabiancgraterol@gmail.com>
2023-02-15 10:43:30 -0400

    turning moviecard into a link

--- src/components/movieCard/movieCard.tsx
+++ src/components/movieCard/movieCard.tsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 import React from "react";
 import {Average} from "@/components/common/Average";
 import {generateImageUrl} from "@/utils/functions/generateImageUrl";
+import Link from "next/link";
+import {MovieInterface} from "@/utils/models/Movies/Movie.interface";
+import {RecommendationInterface} from "@/utils/models/Movies/RecomendationResult.interface";
 
 type props = {
-    data:{
-        poster_path:string,
-        title:string,
-        vote_average:number,
-        popularity:number
-    }
+    data:MovieInterface | RecommendationInterface
 }
 export function MovieCard({data}:props){
     const poster = generateImageUrl(data.poster_path)
     return (
         <article className={styles.movieCard}>
-            <Image src={poster} alt={"title poster"} width={200} height={300}/>
-            <div className={styles.info}>
-                <Average value={data.vote_average} size={"bg"}/>
-            </div>
+            <Link href={`/movie/${data.id}`}>
+                <Image src={poster} alt={"title poster"} width={200} height={300}/>
+                <div className={styles.info}>
+                    <Average value={data.vote_average} size={"bg"}/>
+                </div>
+            </Link>
         </article>
 
     )
