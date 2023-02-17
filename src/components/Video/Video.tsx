3a79323bedc8ed0b6bc556db8c287129aa13cac0
fabian graterol <fabiancgraterol@gmail.com>
2023-02-16 22:02:41 -0400

    rendering iframe depending of state

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,6 +1,6 @@
 import {VideoTrailerInterface} from "@/utils/models/Movies/VideoMedia.interface";
 import style from "./Video.module.css"
-import {ReactNode, useRef, useState} from "react";
+import {ReactNode, useState} from "react";
 
 type props = {
     video:VideoTrailerInterface,
@@ -8,26 +8,19 @@ type props = {
 }
 export function Video({video,children}:props){
     const [show, setShow] = useState(false);
-    const iframeRef  = useRef<HTMLIFrameElement>(null)
 
-    // this is to stop reproduction of iframe each time the video overlay is closed
-    if (!show && iframeRef.current){
-        const src = iframeRef.current.src
-        iframeRef.current.src = src
-    }
     return (
         <div className={style.media}>
             <div
                 className={`${style.video} ${show ? style.show : ""}`}
                 onClick={() => setShow(false)}
             >
-                <iframe
-                    ref={iframeRef}
+                {show && <iframe
                     src={`https://www.youtube.com/embed/${video.key}`}
-                    title={"embed youtube trailer"}
+                    title={video.name}
                     allowFullScreen
                     allow={"encrypted-media; picture-in-picture full"}
-                />
+                />}
             </div>
             <div onClick={() => setShow(true)}>
                 {children}
