1711f57129ffeb9a5a25eaa2caed722581e0abc9
fabian graterol <fabiancgraterol@gmail.com>
2023-03-09 23:18:23 -0400

    solving bug

--- src/utils/functions/formatVideoResponse.ts
+++ src/utils/functions/formatVideoResponse.ts
@@ -2,5 +2,5 @@ import {VideoMediaResponse} from "@/models/Movies/VideoMedia.interface";
 
 export function formatVideoResponse(videos:VideoMediaResponse){
     // returning only the first 9 youtube videos
-    return videos.results.filter(t => t.site == "YouTube").slice(0,9)
+    return videos?.results.filter(t => t.site == "YouTube").slice(0,9) || []
 }
