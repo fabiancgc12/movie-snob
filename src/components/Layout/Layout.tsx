435c1eb9a2a423f1cc61245d8e372fdb1a346e99
fabian graterol <fabiancgraterol@gmail.com>
2023-03-16 14:57:19 -0400

    adding navbar(1/2)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,19 +2,21 @@ import {ReactNode} from "react";
 import {Footer} from "@/components/Layout/Footer";
 import { NavBar } from "./NavBar";
 import {Header} from "@/components/Layout/Header";
+import styles from "./layout.module.css"
 
 type props = {
     children:ReactNode
 }
+
 export function Layout({children}:props){
     return (
-        <>
+        <div className={styles.pageContainer}>
             <Header/>
             <NavBar/>
             <main>
                 {children}
             </main>
             <Footer/>
-        </>
+        </div>
     )
 }
--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,12 +1,50 @@
+import styles from "./NavBar.module.css"
+import {FaBookmark} from "react-icons/fa";
+import {AiFillHeart} from "react-icons/ai";
+import {BiUserCircle} from "react-icons/bi";
+import {MdLocalMovies} from "react-icons/md";
+import Link from "next/link";
+import Image from "next/image"
+import logo from "@public/logo.png";
+
 type props = {
     className?:string
 }
 
+const navItems = [{
+    icon:<FaBookmark />,
+    label:"Marked"
+},{
+    icon:<AiFillHeart />,
+    label:"Liked"
+},{
+    icon:<MdLocalMovies />,
+    label:"Discover"
+},
+{
+    icon:<BiUserCircle />,
+    label:"User"
+}]
+
 
 export function NavBar({className = ""}:props){
     return (
-        <nav className={`${className}`}>
-            holis
-        </nav>
+        <aside className={`${className} ${styles.aside}`}>
+            <nav className={styles.navigation}>
+                <div className={styles.logo}>
+                    <Link href={"/"} className={styles.link}>
+                        <Image src={logo} alt={"logo"}/>
+                    </Link>
+                </div>
+                {navItems.map(item => (
+                    <div key={`nav-item-${item.label}`} className={styles.item}>
+                        <Link href={"#"} className={styles.link} >
+                            {item.icon}
+                            <p className={styles.label}>{item.label}</p>
+                        </Link>
+                    </div>
+                ))}
+            </nav>
+        </aside>
     )
 }
