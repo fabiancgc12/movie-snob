07fb3c30e54cb805769c3a24871012e84a12d3f3
fabian graterol <fabiancgraterol@gmail.com>
2023-03-21 13:49:02 -0400

    creating header (1/2)

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,10 @@
 import {useShowNavBarContext} from "@/global/ShowNavbarContext";
 import {CiMenuBurger} from "react-icons/ci";
 import styles from "./header.module.css"
+import {useRef, useState} from "react";
+import {ActionButton} from "@/components/common/ActionButton/ActionButton";
+import { BiSearch } from "react-icons/bi";
+import useClickOutside from "@/hooks/useClickOutside";
 
 type props = {
     className?:string
@@ -8,16 +12,25 @@ type props = {
 
 export function Header({className = ""}:props){
     const [showNavBar,setShowNavBar] = useShowNavBarContext()
+    const [showSearch,setShowSearch] = useState(false);
+    const searchRef = useRef(null);
+    useClickOutside(searchRef, () => {
+        setShowSearch(false)
+    })
     return (
         <header className={`${className} ${styles.wrapper}`}>
             <button
                 onClick={() => setShowNavBar(!showNavBar)}
-                className={`${styles.openMenu} outline contrast`}
+                className={`${styles.button} outline contrast`}
                     >
                 <CiMenuBurger />
             </button>
-            {/*<button>open</button>*/}
-            header
+            <div>
+                {showSearch && <input type={"text"} placeholder={"search"} ref={searchRef}/>}
+                <ActionButton onClick={() => setShowSearch(true)} size={"xs"}>
+                    <BiSearch/>
+                </ActionButton>
+            </div>
         </header>
     )
 }
