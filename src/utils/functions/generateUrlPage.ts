4ec5eee90dd5e27dbfc1249be51bc9201a4e9bf7
fabian graterol <fabiancgraterol@gmail.com>
2023-03-23 13:52:53 -0400

    fixing link

--- src/utils/functions/generateUrlPage.ts
+++ src/utils/functions/generateUrlPage.ts
@@ -2,5 +2,5 @@ import {TvShowInterface} from "@/models/tv/TvShow.interface";
 import {MovieInterface} from "@/models/Movies/Movie.interface";
 
 export function generateUrlPage(media:TvShowInterface | MovieInterface,mediaType:"tv" | "movie"){
-    return `http://localhost:3000/${mediaType}/${media.id}`
+    return `https://moviesnob.vercel.app/${mediaType}/${media.id}`
 }
