602d44990dffcdfd867d16c8a4b41af791a06de4
fabian <fabiancgraterol@gmail.com>
2026-06-14 16:55:28 -0400

    adding tanstack devtools

--- src/app/[lang]/providers.tsx
+++ src/app/[lang]/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { ThemeProvider } from "@/global/ThemeContext";
 import { ShowNavBarProvider } from "@/global/ShowNavbarContext";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -23,6 +24,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <ThemeProvider>
         <ShowNavBarProvider>{children}</ShowNavBarProvider>
       </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
