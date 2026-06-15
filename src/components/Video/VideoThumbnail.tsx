f9690e0d5a47fe9f8b9fbb21c6db6ece77c9c5dd
fabian <fabiancgraterol@gmail.com>
2026-06-15 01:15:09 -0400

    fixing typescript bugs

--- src/components/Video/VideoThumbnail.tsx
+++ src/components/Video/VideoThumbnail.tsx
@@ -11,38 +11,45 @@ export function VideoThumbnail({ video }: props) {
 
   return (
     <Video video={video}>
-      <div className="relative w-[80vw] max-w-[450px] aspect-[16/9]">
-        <Image
-          src={videoThumbnail}
-          alt={`${video.name} thumbnail`}
-          fill
-          className="rounded-lg hover:brightness-50"
-        />
-        <div className="absolute flex justify-center items-center gap-[5px] top-0 left-0 w-full h-full z-[1] cursor-pointer hover:text-[whitesmoke]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-[25%] h-[25%] transition-colors duration-350"
+      {(props) => (
+        <>
+          <button
+            {...props}
+            className="relative w-[80vw] max-w-[450px] aspect-[16/9]"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            <Image
+              src={videoThumbnail}
+              alt={`${video.name} thumbnail`}
+              fill
+              className="rounded-lg hover:brightness-50"
             />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-            />
-          </svg>
-        </div>
-      </div>
-      <small className="block w-[80vw] max-w-[450px] whitespace-nowrap overflow-hidden text-ellipsis font-bold">
-        {video.name}
-      </small>
+            <div className="absolute flex justify-center items-center gap-[5px] top-0 left-0 w-full h-full z-[1] cursor-pointer hover:text-[whitesmoke]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-[25%] h-[25%] transition-colors duration-350"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                />
+              </svg>
+            </div>
+          </button>
+          <small className="block w-[80vw] max-w-[450px] whitespace-nowrap overflow-hidden text-ellipsis font-bold">
+            {video.name}
+          </small>
+        </>
+      )}
     </Video>
   );
 }
